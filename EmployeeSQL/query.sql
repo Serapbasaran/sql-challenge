8a3b79f40e748a4dc272cb9e1d2da0331deab5f8
Serapbasaran <serap.cbasaran@gmail.com>
2020-11-12 16:39:26 -0500

    7.question

--- EmployeeSQL/query.sql
+++ EmployeeSQL/query.sql
@@ -120,6 +120,15 @@ WHERE departments.dept_name = 'Sales' ;
 -- 7.List all employees in the Sales and Development departments, including their employee number, 
 -- last name, first name, and department name.
 
+SELECT employees.emp_no, employees.last_name, employees.first_name, departments.dept_name
+FROM employees
+INNER JOIN dept_employee ON
+dept_employee.emp_no=employees.emp_no
+INNER JOIN departments ON
+dept_employee.dept_no=departments.dept_no
+WHERE departments.dept_name = 'Sales' OR departments.dept_name='Development';
+
+-- 
 
 
 
