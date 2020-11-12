cc2882d0be454d02aa05ac3ba0f5857103ae42b8
Serapbasaran <serap.cbasaran@gmail.com>
2020-11-12 10:37:41 -0500

    Update query.sql

--- EmployeeSQL/query.sql
+++ EmployeeSQL/query.sql
@@ -1,6 +1,34 @@
+--Create table schemas for each of the six CSV file 
+--Specify data types, primary keys,foreign keys, and other constraints. 
+
+DROP TABLE IF EXISTS Departments;
 CREATE TABLE Departments(
-    Dept_No INT NOT NULL PRIMARY KEY,
-	Dept_Name VARCHAR NOT NULL
+    Dept_No VARCHAR(30) NOT NULL PRIMARY KEY,
+	Dept_Name VARCHAR(30) NOT NULL
 );
 
 SELECT * FROM Departments;
+
+CREATE TABLE Titles(
+   Title_ID VARCHAR(30) PRIMARY KEY,
+   Title VARCHAR(30)
+);
+
+SELECT * FROM Titles;
+
+
+CREATE TABLE Employees(
+    Emp_no INT NOT NULL,
+	Title_ID VARCHAR(30) NOT NULL,
+	Birth_date DATE NOT NULL,
+	First_name VARCHAR(30) NOT NULL,
+	Last_name VARCHAR(30) NOT NULL,
+	Sex VARCHAR(30) NOT NULL,
+	Hire_date DATE NOT NULL,
+	PRIMARY KEY(Emp_no)
+	FOREIGN KEY(Title_ID) REFERENCES Titles (Title_ID);
+);
+
+SELECT * FROM Employees;
+
+
