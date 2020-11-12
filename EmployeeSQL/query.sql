83ddaee19c1a0bde9ffd6919686d93b8a95a7894
Serapbasaran <serap.cbasaran@gmail.com>
2020-11-12 16:13:16 -0500

    3.question

--- EmployeeSQL/query.sql
+++ EmployeeSQL/query.sql
@@ -80,6 +80,19 @@ SELECT employees.first_name, employees.last_name, employees.hire_date
 FROM employees
 WHERE hire_date BETWEEN '1986-01-01' AND '1987-01-01'; 
 
+-- 3.List the manager of each department with the following information: 
+-- department number, department name, the manager's employee number, last name, first name.
+
+SELECT dept_manager.dept_no, departments.dept_name, dept_manager.emp_no, employees.last_name, employees.first_name
+FROM departments
+JOIN dept_manager ON
+departments.dept_no=dept_manager.dept_no
+JOIN employees ON
+dept_manager.emp_no=employees.emp_no
+
+
+
+
 
 
 
